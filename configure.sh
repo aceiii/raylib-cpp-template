656ff7548c165d42bdee9b975981adaf7fdf2319
Borin Ouch <borin.ouch@gmail.com>
2024-11-29 12:14:22 -0800

    Move packages from vcpkg to submodules, add main.cpp, fix build

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-cmake -B build -S . \
-    -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+cmake \
+    -B=build \
+    -S=. \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -G=Ninja
